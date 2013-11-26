ddb31473638be1faa4ca9672af28ac29c4adb2a6
Sho Nakatani <lay.sakura@gmail.com>
2013-11-26 15:29:20 +0900

    cascade test passes Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- relshell/test/test_practicaluse_01.py
+++ relshell/test/test_practicaluse_01.py
@@ -26,70 +26,12 @@ def test_output_batch_as_is():
     eq_(out_batch, in_batch)
 
 
-# def test_output_2in_1out():
-#     batch0 = _create_batch()
-#     rdef   = RecordDef([{'name': 'text', 'type': 'STRING'}])
-#     batch1 = Batch((
-#         Record(rdef, 'test1'),
-#         Record(rdef, 'test2xx'),
-#         Record(rdef, 'test3'),
-#         Record(rdef, 'test4'),
-#     ))
-
-#     op = ShellOperator(
-#         'diff IN_BATCH0 IN_BATCH1 > OUT_BATCH',
-#         out_record_def=RecordDef([
-#             {'name': 'position',
-#              'type': 'STRING',
-#             },
-#             {'name': 'diff',
-#              'type': 'STRING',
-#             },
-#         ]),
-#         # terminatorはいらない．diffは2バッチ比較終わったらそのままexit(0)するから．
-#         # daemonize=Falseなので暗黙のうちにop.kill()が呼ばれるが，
-#         # これは「もうプロセスが死んでいたら何もしない」という動作にする．
-
-#         col_patterns = {   # defaultでは r'^.*$\n' にするって感じ
-#             'position': re.compile(r'''
-#                 # diff position
-#                 (
-#                     \d (,\d)? [acd] \d (,\d)?
-#                 )
-#             ''', re.VERBOSE | re.MULTILINE),
-
-#             'diff': re.compile(r'''
-#                 # diff content
-#                 (
-#                     (^[<>] .*?$\n)+
-#                     (
-#                         ^---$\n
-#                         (^> .*?$\n)*
-#                     )?
-#                 )
-#             ''', re.VERBOSE | re.MULTILINE)
-#         }
-#     )
-#     out_batch = op.run(in_batches=(batch0, ))
-#     batch2 = op.run(in_batches=(batch1, ))
-#     eq_(batch2, batch0)
-
-
-# def test_daemonized_process():
-#     op = ShellOperator(
-#         'cat < IN_BATCH0 > OUT_BATCH',
-#         out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-#         terminator     = Terminator.EOF  # Terminator.SIGKILL, ...
-#         daemonize      = True,  # op.kill() を明示的に呼び出すまでterminatorの実行がなされない
-#     )
-#     for i in xrange(10):
-#         in_batch  = _create_batch()
-#         out_batch = op.run(in_batches=(in_batch, ))   # daemonize=Trueなので，シェルプロセスが走り続けていても，
-#                                                 # 1個のin_batchについての処理が終わればrun呼び出しは終わる
-#                                                 # (1個のin_batchについて同期呼び出し)
-#                                                 # ただし，「どこまでが1つのoutput_batchか」を判定する必要はある．
-#                                                 # 「空行が1つ出たら」とか，「何ms新しい出力がなかったら」とか?
-#         eq_(out_batch, in_batch)
-#     op.kill()  # daemonize=Trueのときのkillし忘れを何とかしたい．subprocessがゾンビになっちゃうし．
-#                # デストラクタで警告出しつつkill -9送る?
-#                # というか，kill()みたいに「呼び出さないといけない」インターフェースよりwith構文使うべきな感じがある
+def test_output_batch_cascade():
+    op = ShellOperator(
+        'cat < IN_BATCH0 > OUT_BATCH',
+        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
+    )
+    batch_a = _create_batch()
+    batch_b = op.run(in_batches=(batch_a, ))
+    batch_c = op.run(in_batches=(batch_b, ))
+    eq_(batch_c, batch_a)
