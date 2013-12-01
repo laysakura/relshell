591ec16090c6df24db78708388541154c71592ca
Sho Nakatani <lay.sakura@gmail.com>
2013-12-01 11:12:58 +0900

    parameterized tests Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- relshell/test/test_shelloperator.py
+++ relshell/test/test_shelloperator.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from nose.tools import *
+from nose_parameterized import parameterized
 from relshell.recorddef import RecordDef
 from relshell.record import Record
 from relshell.batch import Batch
 from relshell.shelloperator import ShellOperator
+
+
+def _simple_recdef():
+    return RecordDef([{'name': 'text', 'type': 'STRING'}])
 
 
 def _create_batch():
@@ -16,14 +21,66 @@ def _create_batch():
     ))
 
 
-def test_output_batch_as_is():
+@parameterized([
+    # ( <simple command w/ RecordDef([{'name': 'text', 'type': 'STRING'}]) in/out> )
+    ('cat < IN_BATCH0 > OUT_BATCH'),
+    ('cat   IN_BATCH0 > OUT_BATCH'),
+    # ('tee < IN_BATCH0 OUT_BATCH'),
+])
+def test_simple_operator(cmd):
     op = ShellOperator(
-        'cat < IN_BATCH0 > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
+        cmd,
+        out_record_def=_simple_recdef(),
     )
     in_batch  = _create_batch()
     out_batch = op.run(in_batches=(in_batch, ))
     eq_(out_batch, in_batch)
+
+
+@parameterized([
+    # ( <simple command w/ RecordDef([{'name': 'text', 'type': 'STRING'}]) in/out> )
+    ('cat < IN_BATCH0 > OUT_BATCH'),
+    ('cat   IN_BATCH0 > OUT_BATCH'),
+])
+@raises(AttributeError)
+def test_simple_operator_batch_mismatch(cmd):
+    op = ShellOperator(
+        cmd,
+        out_record_def=_simple_recdef(),
+    )
+    in_batch0  = _create_batch()
+    in_batch1  = _create_batch()
+    op.run(in_batches=(in_batch0, in_batch1))
+
+
+@parameterized([
+    # ( <simple command w/ RecordDef([{'name': 'text', 'type': 'STRING'}]) in/out> )
+    ('cat < IN_BATCH0 > OUT_BATCH'),
+    ('cat   IN_BATCH0 > OUT_BATCH'),
+])
+@raises(AttributeError)
+def test_simple_operator_batch_mismatch(cmd):
+    op = ShellOperator(
+        cmd,
+        out_record_def=_simple_recdef(),
+    )
+    in_batch0  = _create_batch()
+    in_batch1  = _create_batch()
+    op.run(in_batches=(in_batch0, in_batch1))
+
+
+@parameterized([
+    # ( <simple command w/ RecordDef([{'name': 'text', 'type': 'STRING'}]) in/out> )
+    ('cat /no/such/file > OUT_BATCH'),
+    ('wiredcmd > OUT_BATCH'),
+])
+@raises(OSError)
+def test_simple_operator_error_cmd(cmd):
+    op = ShellOperator(
+        cmd,
+        out_record_def=_simple_recdef,
+    )
+    op.run(in_batches=())
 
 
 def test_output_batch_cascade():
@@ -35,16 +92,6 @@ def test_output_batch_cascade():
     batch_b = op.run(in_batches=(batch_a, ))
     batch_c = op.run(in_batches=(batch_b, ))
     eq_(batch_c, batch_a)
-
-
-def test_output_batch_as_is_file():
-    op = ShellOperator(
-        'cat IN_BATCH0 > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-    )
-    in_batch  = _create_batch()
-    out_batch = op.run(in_batches=(in_batch, ))
-    eq_(out_batch, in_batch)
 
 
 def test_output_batch_sorted():
@@ -68,43 +115,3 @@ def test_output_batch_sorted():
     in_batch  = _create_batch()
     out_batch = op.run(in_batches=(in_batch, ))
     eq_(out_batch, sorted_batch)
-
-
-@raises(AttributeError)
-def test_num_in_batch_missmatch_file():
-    op = ShellOperator(
-        'cat IN_BATCH0  > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-    )
-    in_batch0  = _create_batch()
-    in_batch1  = _create_batch()
-    op.run(in_batches=(in_batch0, in_batch1))
-
-
-@raises(AttributeError)
-def test_num_in_batch_missmatch_stdin():
-    op = ShellOperator(
-        'cat < IN_BATCH0  > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-    )
-    in_batch0  = _create_batch()
-    in_batch1  = _create_batch()
-    op.run(in_batches=(in_batch0, in_batch1))
-
-
-@raises(OSError)
-def test_output_batch_error_cmd1():
-    op = ShellOperator(
-        'cat /no/such/file > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-    )
-    op.run(in_batches=())
-
-
-@raises(OSError)
-def test_output_batch_error_cmd2():
-    op = ShellOperator(
-        'wiredcmd > OUT_BATCH',
-        out_record_def = RecordDef([{'name': 'text', 'type': 'STRING'}]),
-    )
-    op.run(in_batches=())
