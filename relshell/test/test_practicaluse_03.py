40eb4f52d6326e80cc7ce8c6e5c5a38f52b4a29f
Sho Nakatani <lay.sakura@gmail.com>
2013-12-02 16:35:03 +0900

    adds diff test code Signed-off-by: Sho Nakatani <lay.sakura@gmail.com>

--- relshell/test/test_practicaluse_03.py
+++ relshell/test/test_practicaluse_03.py
@@ -7,8 +7,23 @@ from relshell.batch import Batch
 from relshell.shelloperator import ShellOperator
 
 
+def _simple_recdef():
+    return RecordDef([{'name': 'text', 'type': 'STRING'}])
+
+
+def _diff_recdef():
+    return RecordDef([
+        {'name': 'position',
+         'type': 'STRING',
+        },
+        {'name': 'diff',
+         'type': 'STRING',
+        },
+    ])
+
+
 def _create_batch():
-    rdef = RecordDef([{'name': 'text', 'type': 'STRING'}])
+    rdef = _simple_recdef()
     return Batch((
         Record(rdef, 'test1'),
         Record(rdef, 'test2'),
@@ -17,26 +32,23 @@ def _create_batch():
     ))
 
 
-def test_output_2in_1out():
-    batch0 = _create_batch()
-    rdef   = RecordDef([{'name': 'text', 'type': 'STRING'}])
-    batch1 = Batch((
+def _create_batch_diff():
+    rdef = _simple_recdef()
+    return Batch((
         Record(rdef, 'test1'),
         Record(rdef, 'test2xx'),
         Record(rdef, 'test3'),
         Record(rdef, 'test4yy'),
     ))
 
+
+def test_output_2in_1out():
+    batch0 = _create_batch()
+    batch1 = _create_batch_diff()
+
     op = ShellOperator(
         'diff IN_BATCH0 IN_BATCH1 > OUT_BATCH',
-        out_record_def=RecordDef([
-            {'name': 'position',
-             'type': 'STRING',
-            },
-            {'name': 'diff',
-             'type': 'STRING',
-            },
-        ]),
+        out_record_def=_diff_recdef(),
         success_exitcodes=(0, 1),
         out_col_patterns={   # defaultでは r'^.*$\n' にするって感じ
             'position': re.compile(r'''
@@ -59,7 +71,27 @@ def test_output_2in_1out():
         }
     )
     out_batch = op.run(in_batches=(batch0, batch1))
-    print(out_batch)
+    eq_(out_batch,
+        Batch((
+            Record(
+                _diff_recdef(),
+                '2c2',
+'''< test2
+---
+> test2xx
+''',
+            ),
+
+            Record(
+                _diff_recdef(),
+                '4c4',
+'''< test4
+---
+> test4yy
+''',
+            ),
+        ))
+    )
 
 
 def test_output_batch_cascade():
