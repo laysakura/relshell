1be9a24fffedb6e29313c3b0cf8b683798987f22
Sho Nakatani <lay.sakura@gmail.com>
2014-01-23 00:57:00 +0900

    adds TIMESTAMP type

--- relshell/test/test_type.py
+++ relshell/test/test_type.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
-from nose.tools import *
+import nose.tools as ns
+import datetime as dt
 from relshell.type import Type
+from relshell.timestamp import Timestamp
 
 
 def test_type_usage():
-    eq_(str(Type('STRING')), 'STRING')
-    eq_(Type.equivalent_relshell_type(-123), Type('INT'))
+    ns.eq_(str(Type('STRING')), 'STRING')
+    ns.eq_(Type.equivalent_relshell_type(-123), Type('INT'))
+
+
+def test_types():
+    ns.eq_(Type.equivalent_relshell_type(-123)                         , Type('INT'))
+    ns.eq_(Type.equivalent_relshell_type('123')                        , Type('STRING'))
+    ns.eq_(Type.equivalent_relshell_type(Timestamp(dt.datetime.now())) , Type('TIMESTAMP'))
 
 
 def test_cast():
-    eq_(Type('INT').   python_cast('12345'), 12345)
-    eq_(Type('STRING').python_cast(6789),    '6789')
+    ns.eq_(Type('INT').   python_cast('12345'), 12345)
+    ns.eq_(Type('STRING').python_cast(6789),    '6789')
 
 
-@raises(NotImplementedError)
+@ns.raises(NotImplementedError)
 def test_unsupported_type_init():
     Type('UNSUPPORTED_TYPE')
 
 
-@raises(NotImplementedError)
+@ns.raises(NotImplementedError)
 def test_unsupported_type_equivalent():
     class X:
         pass
